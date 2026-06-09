e975b4907ff11775e8a5500bc49af0433b6caeca
Razor1080p <140715106+razor1009@users.noreply.github.com>
2026-06-09 15:37:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,3 +20,17 @@ io.on("connection", (socket) => {
 http.listen(3000, () => {
     console.log("Server running on port 3000");
 });
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+http.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
